612dbcda941625a6469fd74af29eef5ce8ab5fb0
Victor Vale <joaovictorvale06@gmail.com>
2024-05-29 15:48:56 -0300

    bugfix: Alterando validation de empresa

--- src/Senium.Domain/Validations/EmpresaValidation.cs
+++ src/Senium.Domain/Validations/EmpresaValidation.cs
@@ -12,14 +12,14 @@ public class EmpresaValidation : AbstractValidator<Empresa>
             .Matches("^[a-zA-ZÀ-ÿ\\s]+$").WithMessage("O campo Nome deve conter apenas letras.")
             .MaximumLength(100).WithMessage("O campo Nome deve conter no máximo 100 caracteres.");
         
-        RuleFor(e => e.Email)
+        RuleFor(u => u.Email)
             .NotEmpty().WithMessage("O campo E-mail é obrigatório.")
             .EmailAddress().WithMessage("O campo E-mail deve ser um endereço de e-mail válido.");
 
         RuleFor(e => e.Telefone)
             .NotEmpty().WithMessage("O campo Telefone é obrigatório.")
-            .Matches(@"^[\d\(\)]+$").WithMessage("O campo Telefone deve conter apenas números e parênteses.")
-            .Length(13, 13).WithMessage("O campo Telefone deve conter 13 caracteres incluindo o DDD: (21)912345678");
+            .Matches(@"^\(\d{2}\)\d{8}$").WithMessage("O campo Telefone deve estar no formato correto: (XX)XXXXXXXX")
+            .Length(12, 13).WithMessage("O campo Telefone deve conter entre 12 e 13 caracteres incluindo o DDD: (XX)XXXXXXXX");
 
         RuleFor(e => e.NomeDaEmpresa)
             .NotEmpty().WithMessage("O campo Nome da Empresa é obritgatório.");
