af6446a9fbc2da4894222479dd186fa1c7ac09ec
João Victor Vale Fernandes <joaovictorvale06@gmail.com>
2024-06-24 23:36:50 +0000

    Revert "Atualizações dos tests unitários. Finalizando."

--- src/Senium.Core/Settings/EmailSettings.cs
+++ src/Senium.Core/Settings/EmailSettings.cs
@@ -3,7 +3,7 @@
 public class EmailSettings
 {
     public string Name { get; set; } = String.Empty;
-    public string User { get; set; } = String.Empty;
+    public string User  { get; set; } = String.Empty;
     public string Password { get; set; } = String.Empty;
     public string Server { get; set; } = String.Empty;
     public int Port { get; set; }
